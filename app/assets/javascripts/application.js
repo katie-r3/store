3037cd395f1151bc6b1ecb680c52ac5e59a8fe92
Katie Dodd <katie@RubyThrees-iMac.attlocal.net>
2017-09-11 17:54:22 -0700

    created store site, can add items to cart, but can't remove them

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,61 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery-ui
+//= require rails-ujs
+//= require_tree .
+
+$(document).ready(function() {
+
+
+  return $('a[data-target]').click(function(e) {
+    var $this, new_target, url;
+    e.preventDefault();
+    $this = $(this);
+    if ($this.data('target') === 'Add to') {
+      url = $this.data('addurl');
+      new_target = "Remove from";
+    } else {
+      url = $this.data('removeurl');
+      new_target = "Add to";
+    }
+    return $.ajax({
+      url: url,
+      type: 'put',
+      success: function(data) {
+        $('.cart-count').html(data);
+        $this.find('span').html(new_target);
+        return $this.data('target', new_target);
+      }
+    });
+  });
+
+
+  $('#mycart .fi-x').click(function(e) {
+  var $this, url;
+  e.preventDefault();
+  $this = $(this).closest('a');
+  url = $this.data('targeturl');
+  return $.ajax({
+    url: url,
+    type: 'put',
+    success: function(data) {
+      $('.cart-count').html(data);
+      return $this.closest('.cart-item').slideUp();
+      if (data.status == 'ERROR') {
+        console.log("There was an error.")
+      }
+    }
+  });
+});
+
+});
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+  resource :cart, only: [:show] do
+    put 'add/:item_id', to: 'carts#add', as: :add_to
+    put 'remove/:item_id', to: 'carts#remove', as: :remove_from
+  end
+
+  root 'items#index'
+
+  devise_for :users
+
+  resources :items
+
+end
