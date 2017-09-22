0fb0ad1c43335d4a25c3b5844292c082b9387e7c
Katie Dodd <katie@RubyThrees-iMac.attlocal.net>
2017-09-22 13:02:33 -0700

    deployed to heroku, however, the state isn't being saved to the user

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -22,4 +22,15 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :state, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :state, :password, :password_confirmation, :current_password)
+  end
+  
 end
