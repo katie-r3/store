8418efb0bb0d60d3f4369b3492e3a0510e71dbb5
Katie Dodd <katie@RubyThrees-iMac.attlocal.net>
2017-09-29 12:15:46 -0700

    thought the heroku image issue was fixed, but it wasn't, trying again

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
 
   # GET /resource/sign_in
   # def new
@@ -22,5 +23,11 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:state])
   # end
+
+  private
+
+  def set_s3_direct_post
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+  end
 
 end
