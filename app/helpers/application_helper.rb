e8acccfdecf7b6eae5ea1a5ce187e3ca2b7871a0
Katie Dodd <katie@RubyThrees-iMac.attlocal.net>
2017-10-10 17:26:10 -0700

    can't seem to customize the notices, tried everything I could think of

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def flash_class(level)
+    case level
+    when :notice then "alert-box"
+    when :success then "alert-box success"
+    when :alert then "alert-box alert round"
+    end
+  end
+
 end
