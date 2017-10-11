35ae48c878f9207c95092571f6b2a9d2f8c2ef48
Katie Dodd <katie@RubyThrees-iMac.attlocal.net>
2017-10-11 16:21:35 -0700

    changed the user's purchases page

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,19 @@
 Item.delete_all
 
 item_list = [
-  ["phonecase", 12.99, "galaxy s8 phone case", 10],
-  ["phone charger", 12.99, "charger for iphone 7", 10],
-  ["water bottle", 12.99, "steel water bottle", 10],
-  ["mouse", 12.99, "mouse for mac", 10, "MLA02.jpeg"],
-  ["backpack", 12.99, "backpack with room for laptop", 10],
-  ["galaxy s8", 399.99, "s8 galaxy phone", 10],
-  ["iphone 7", 359.99, "iphone 7", 10],
-  ["mousepad", 12.99, "silly kitty mousepad", 10],
-  ["kombucha", 12.99, "variety pack of synergy kombucha", 10],
-  ["mouse", 9.99, "cheap mouse", 10],
-  ["stuff", 4.49, "lots of stuff", 10],
-  ["snacks", 9.99, "variety of snacks", 10],
-  ["phone charger", 14.99, "charger for galaxy s8", 10]
+  ["phonecase", 12.99, "Galaxy S8 phonecase", 10],
+  ["phone charger", 12.99, "Charger for iPhone 7", 10],
+  ["water bottle", 12.99, "Steel water bottle", 10],
+  ["mouse", 12.99, "Wireless Mac mouse", 10, "MLA02.jpeg"],
+  ["backpack", 12.99, "Backpack with flap for laptop", 10],
+  ["Galaxy S8", 399.99, "S8 Galaxy phone", 10],
+  ["iPhone 7", 359.99, "iPhone 7", 10],
+  ["mousepad", 12.99, "Silly kitty mousepad", 10],
+  ["kombucha", 12.99, "Variety pack of Synergy kombucha", 10],
+  ["mouse", 9.99, "Wireless mouse, compatible with any computer", 10],
+  ["water bottle", 4.49, "Glass water bottle(colors may vary)", 10],
+  ["snack pack", 9.99, "Snack Pack - not guaranteed fresh", 10],
+  ["phone charger", 14.99, "Charger for Galaxy S8", 10]
 ]
 
 item_list.each do |name, price, description, quantity|
