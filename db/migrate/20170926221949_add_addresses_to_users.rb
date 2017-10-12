115b896c88f6d98481fb5a4ab3cec38943633aeb
Katie Dodd <katie@RubyThrees-iMac.attlocal.net>
2017-10-12 11:42:03 -0700

    fixing migration problem on heroku

--- db/migrate/20170926221949_add_addresses_to_users.rb
+++ db/migrate/20170926221949_add_addresses_to_users.rb
@@ -1,7 +1,6 @@
 class AddAddressesToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :address_line1, :string
-    add_column :users, :address_line2, :string
+    add_column :users, :address, :string
     add_column :users, :city, :string
   end
 end
