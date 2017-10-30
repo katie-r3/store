d1b5c72960d66b4399a037c4b13cd962446b23bb
Katie Dodd <katie@RubyThrees-iMac.attlocal.net>
2017-10-30 12:24:48 -0700

    fixed forgot password issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-
+  Rails.application.routes.default_url_options[:host] = 'domain.com'
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "store_#{Rails.env}"
 
+  Rails.application.routes.default_url_options[:host] = 'domain.com'
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
